74673293b9e1edc4b1464549ecd10a9a73e0b2a1
gaby <annagabriellaferreira2008@gmail.com>
2021-04-18 14:50:30 -0300

    exercicio 97 ui ui :D

---
+++ 97.py
@@ -0,0 +1,8 @@
+def escreva(msg):
+    tamanho_uiui = len(msg)
+    print('*' * tamanho_uiui)
+    print(msg)
+    print('*' * tamanho_uiui)
+
+
+escreva('se você ama alguem deixe-o ir. se ele ao retornar, procure-o e mate-o- leonardo lake')
