2765f51c7d4759f1774fef89fb9a788cd4ff7ab4
aby <annagabriellaferreira2008@gmail.com>
2021-06-27 16:31:21 -0300

    projeto final (rascunho)

---
+++ lost_city.py
@@ -0,0 +1,5 @@
+ilha1 = input('Emma - ei, capitão!, Aquilo alí parece uma ilha. devemos parar o navio? sim/não')
+ if ilha1 == 'sim':
+  print('PIRATAS!. vamos procurar comidas mantimentos armas etc... NÃO SE SEPAREM OK? piratas- SIM SENHOR CAPITÃO!!!!.')
+ else:
+  print('*tempestade*')
