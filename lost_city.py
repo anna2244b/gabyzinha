37f276c2144b4f017f0051f4b743704f95b012aa
aby <annagabriellaferreira2008@gmail.com>
2021-06-27 16:39:50 -0300

    projeto final (rascunho) de verdade dessa vez obg

--- lost_city.py
+++ lost_city.py
@@ -1,5 +1,11 @@
 ilha1 = input('Emma - ei, capitão!, Aquilo alí parece uma ilha. devemos parar o navio? sim/não')
+while True:
  if ilha1 == 'sim':
   print('PIRATAS!. vamos procurar comidas mantimentos armas etc... NÃO SE SEPAREM OK? piratas- SIM SENHOR CAPITÃO!!!!.')
  else:
   print('*tempestade*')
+ from random import randint
+ tempestade = randint(0, 3)
+ if tempestade == '2':
+   print('Oops! you died.')
+ break
