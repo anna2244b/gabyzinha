3cd0c03ac365b55d18e9ee93e51646658f3928ee
Notebook HP <annagabriellaferreira2008@gmail.com>
2021-07-14 15:10:44 -0300

    parte do projeto final

--- projeto_finall.py
+++ projeto_finall.py
@@ -2,11 +2,16 @@ ilha1 = input('Emma - ei, capitão!, Aquilo alí parece uma ilha. devemos parar 
 while True:
  if ilha1 == 'sim':
   print('PIRATAS!. vamos procurar comidas mantimentos armas etc... NÃO SE SEPAREM OK? piratas- SIM SENHOR CAPITÃO!!!!.')
-
+  monstro = 11
+  usuario = input('digite um numero de 1 a 12: ')
+  if usuario <= monstro:
+   print('voce achou o mapa')
+     print('voltar para navio')
+  else:
+   print('opss!you died.')
  else:
   print('Seu navio entrou em uma grande tempestade')
- from random import randint
- tempestade = randint(0, 3)
+ tempestade = from random import randint(0, 3)
  if tempestade == '2':
    print('Oops! you died.')
  else:
